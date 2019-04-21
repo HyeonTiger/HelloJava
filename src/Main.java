ed54824d6bfec7daca9ca17ec3da3211d55da772
HyeonTiger <49827360+HyeonTiger@users.noreply.github.com>
2019-04-21 13:55:31 +0900

    Sum Formula Test
    Sum Test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num1 = 3;
+		int num2 = 5;
+		
+		System.out.println("Result : " + (num1 + num2));
+		
 
 	}
 
